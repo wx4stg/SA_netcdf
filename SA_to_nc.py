b8de26adb3abd5a9b778c1de5f15d43e8ceefd27
Sam Gardner <sam@wx4stg.com>
2024-09-27 15:06:34 -0500

    clean up, remove jupyter bones

--- SA_to_nc.py
+++ SA_to_nc.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-from time import sleep
 import numpy as np
 import xarray as xr
-import matplotlib.pyplot as plt
 import struct
 import datetime
-import sys
 from glob import glob
 import scipy.signal as signal
 from scipy.signal import medfilt
 import pandas as pd
 import datetime
-import matplotlib.dates as mdates
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
-import csv 
-from pandas.core.common import flatten
-
-
-# In[2]:
 
 
 SAMPLE_RATE = 10000  # Hertz
 u4max = 4294967295
 
 SAPATH = "/Users/coogray/Documents/SA_DATA/Jun_15_2023/"
-
-
-# In[4]:
 
 
 import time
@@ -60,8 +45,6 @@ def tic():
     toc(False)
 
 
-# In[5]:
-
 
 def convert_adc_to_decimal(value):
     modulo = 1 << 24
@@ -72,11 +55,7 @@ def convert_adc_to_decimal(value):
 SERIAL_SPEED = 2000000
 
 
-# In[6]:
-
-
 def decode_data_packet(mp):
-    # print(mp)
     result = dict()
     result['start_byte'] = struct.unpack('B', mp[0:1])[0]
     result['b1'] = struct.unpack('B', mp[1:2])[0]
@@ -91,8 +70,6 @@ def decode_data_packet(mp):
     adc_ba += mp[3:4]
     adc_ba += b'\x00'
 
-    # print(mp[3:4])
-    # print(adc_ba)
     adc_reading = struct.unpack('>i', adc_ba[:])[0]
 
     adc_reading = mp[1]
@@ -103,34 +80,18 @@ def decode_data_packet(mp):
 
     result['adc_reading'] = adc_reading
     return result
+
 def notch_sixty(s, fs):
     f0 = 60.0  # Frequency to be removed from signal (Hz)
     Q = 2.0  # Quality factor = center / 3dB bandwidth
     b, a = signal.iirnotch(f0, Q, fs)
     return signal.filtfilt(b, a, s)
 
-# def notch_sixty(s, fs):
-#     f0 = 0.125  # Frequency to be removed from signal (Hz)
-#     Q = 0.5 #2.0  # Quality factor = center / 3dB bandwidth
-#     b, a = signal.iirnotch(f0, Q, fs)
-#     return signal.filtfilt(b, a, s)
-
-
-# # Preprocessing - raw to NetCDF
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
 
 
 def compress_all(nc_grids, min_dims=1):
     for var in nc_grids:
         if len(nc_grids[var].dims) >= min_dims:
-            # print("Compressing ", var)
             nc_grids[var].encoding["zlib"] = True
             nc_grids[var].encoding["complevel"] = 5
             nc_grids[var].encoding["contiguous"] = False
@@ -188,11 +149,9 @@ def correct_micros(micros, fs, tol=0.1, window=7):
     # Allow some tolerance
     big_neg_thresh = -int((1-tol)*u4max)
     sample_delta = np.diff(med_micro)
-    # print(big_neg_thresh, "negative rollover threshold; ", sample_delta.min(), "lowest delta")
     large_neg = np.hstack([[False], 
                            (sample_delta < big_neg_thresh)])
     n_rollovers = large_neg.sum()
-    # print(n_rollovers, "total rollovers")
     
     # Set the points with large negative deltas to uint4max,
     # then cumsum will accumulate correction amount to all points
@@ -266,10 +225,6 @@ def interpolate_across_system_times(ds, SAMPLE_RATE=10000):
     ds['time'] = ds.time_orig_method - (correction*1e9).astype('timedelta64[ns]')
     return ds
 
-
-# In[14]:
-    
-# In[14]:
     
 def add_other_time_vars(ds):
     ds['dt_system'] = (ds.time_orig_method-ds.time_orig_method[0]).astype('datetime64[ns]').astype('f8')/1e9
@@ -345,109 +300,8 @@ for _idx, filename in enumerate(sorted(files)):
          'time_orig_method':time_orig})).reset_index('dim_0').drop_vars('dim_0').rename_dims({'dim_0':'sample'})
     
     ds = interpolate_across_system_times(add_other_time_vars(ds))
-    
-    # all_data.append(ds)
-    
-    # if T0.hour != prev_hr:
-    #     prev_hr = T0.hour
-    #     print('hour roll over')
-    #     do_write = True
 
-    # last_hour_chunk = datetime(T0.year, T0.month, T0.day, T0.hour) - timedelta(hours=1)
-    # # data will eventually go in this file ...
-    # last_hour_string = last_hour_chunk.strftime('%Y%m%d_%H')
-    # fileoutname = last_hour_string+"_"+sensorname+"_"+os.path.basename(files[0])[:-4]+".nc"
-    
     # fileoutname = T0.strftime("%Y%m%d%H%M%S_%f")+"-"+sensorname+"-"+os.path.basename(filename)[0][:-4]+".nc"
     fileoutname = T0.strftime("%Y%m%d%H%M%S_%f")+"_"+sensorname+"_"+os.path.basename(filename)[23:-4]+".nc"
     comp_ds = compress_all(ds)
-    # print(comp_ds)
     comp_ds.to_netcdf(fileoutname)
-
-    # if do_write:
-    #     # Write all but the last chunk read, and carry that over to the next hour segment.
-    #     ds_all = xr.concat([d.reset_index('dim_0').drop('dim_0') for d in all_data[:-1]], dim='dim_0').rename_dims({'dim_0':'sample'})
-    #     compress_all(ds_all).to_netcdf(fileoutname)
-    #     all_data = all_data[-1:]
-    #     print(all_data[0].time_orig_method.values[0])
-    #     do_write = False
-
-# write any remaining data
-# ds_all = xr.concat([d.reset_index('dim_0').drop('dim_0') for d in all_data], dim='dim_0').rename_dims({'dim_0':'sample'})
-# compress_all(ds_all).to_netcdf(fileoutname)
-
-
-# In[15]:
-
-
-ds = xr.open_mfdataset(sorted(glob('./202306*-sensor3-*.nc')), combine='nested', concat_dim='sample')
-
-
-# In[16]:
-
-
-ds
-
-
-# In[10]:
-
-
-# %matplotlib widget
-# plt.close('all')
-# fig, axs = plt.subplots(2,1)
-# ds.pps_micro.plot(ax=axs[0])
-# ds.time_orig_method.plot(ax=axs[1])
-# fig.tight_layout()
-
-
-# In[11]:
-
-
-# ds['dt_system'] = (ds.time_orig_method-ds.time_orig_method[0]).astype('datetime64[ns]').astype('f8')/1e9
-# ds['dt_adc'] = (ds.pps_micro - ds.pps_micro[0]).astype('f8')/1e6
-
-# ds['system_sample_dt'] = (ds.time_orig_method-ds.time_orig_method[0]).astype('datetime64[ns]').astype('f8')/1e9
-# ds['adc_sample_dt'] = (ds.pps_micro - ds.pps_micro[0]).astype('f8')/1e6
-
-# # ds['dt_adc']=(dt_micro.values).astype('timedelta64[us]')
-
-
-# In[12]:
-
-
-# %matplotlib widget
-# fig, axs = plt.subplots(2,1)
-# ds.dt_system.plot(ax=axs[0])
-# ds.dt_adc.plot(ax=axs[1])
-# # axs[2].plot(np.diff(ds.time_orig_method.astype('i8').values)/1e9)
-# fig.tight_layout()
-
-
-# In[42]:
-
-
-# t_axis_data = ds['dt_adc']
-
-# fig, ax = plt.subplots(2,1, sharex=True)
-# ax[0].plot(t_axis_data,(ds['dt_adc']-ds['dt_system']))
-# ax[0].set_xlabel('ADC clock elapsed')
-# ax[0].set_ylabel('System clock - ADC clock (s)')
-
-# ax[1].plot(t_axis_data, medfilt(ds['ADC'].values, 7))
-# ax[1].set_xlabel('ADC clock elapsed (s)')
-# ax[1].set_ylabel('ADC (DN)')
-
-# fig.tight_layout()
-
-
-# In[43]:
-
-
-# ds
-
-
-# In[ ]:
-
-
-
-
